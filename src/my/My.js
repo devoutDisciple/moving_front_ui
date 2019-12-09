4ff588d51024009e76ca04b4c66bf9c362518141
zhangzhen <1094705507@qq.com>
2019-12-10 01:23:19 +0800

    增加图片选择器

--- src/my/My.js
+++ src/my/My.js
@@ -1,14 +1,41 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View} from 'react-native';
-// import {Avatar} from 'react-native-elements';
 import {Button, Avatar} from 'react-native-elements';
+import ImagePicker from 'react-native-image-picker';
 
+//图片选择器参数设置
+const options = {
+    title: '请选择图片',
+    cancelButtonTitle: '取消',
+    takePhotoButtonTitle: '拍照',
+    chooseFromLibraryButtonTitle: '相册图片',
+    // customButtons: [{name: '自定义', title: '自定义图片'}],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images',
+    },
+};
 export default class MyScreen extends React.Component {
     componentDidMount() {}
 
     choosePic() {
         console.log(123);
+        ImagePicker.showImagePicker(options, response => {
+            console.log(response, 11);
+            if (response.didCancel) {
+                console.log('用户取消了选择！');
+            } else if (response.error) {
+                alert('ImagePicker发生错误：' + response.error);
+            } else if (response.customButton) {
+                alert('自定义按钮点击：' + response.customButton);
+            } else {
+                let source = {uri: response.uri};
+                this.setState({
+                    avatarSource: source,
+                });
+            }
+        });
     }
 
     render() {
