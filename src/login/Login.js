0e5f661c5b11a7c80579681cce0eea5838862af4
zhangzhen <1094705507@qq.com>
2019-12-11 22:49:20 +0800

    添加icons

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,8 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View} from 'react-native';
-// import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import {Fumi} from 'react-native-textinput-effects';
+import {Sae} from 'react-native-textinput-effects';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+// import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class LoginScreen extends React.Component {
     render() {
@@ -10,6 +11,7 @@ export default class LoginScreen extends React.Component {
             <View
                 style={{
                     flex: 1,
+                    backgroundColor: 'red',
                 }}>
                 <Text>登录页面</Text>
                 <Text
@@ -18,14 +20,18 @@ export default class LoginScreen extends React.Component {
                     }}>
                     返回
                 </Text>
-                <Fumi
-                    label="Course Name"
-                    // iconClass={FontAwesomeIcon}
-                    iconName="university"
-                    iconColor="#f95a25"
-                    iconSize={20}
-                    iconWidth={40}
+                <Sae
+                    label={'姓名'}
+                    iconClass={FontAwesomeIcon}
+                    iconName={'pencil'}
+                    iconColor={'white'}
                     inputPadding={16}
+                    labelHeight={24}
+                    // active border height
+                    borderHeight={2}
+                    // TextInput props
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
                 />
             </View>
         );
