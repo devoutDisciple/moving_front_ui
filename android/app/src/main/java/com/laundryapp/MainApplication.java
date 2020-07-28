d3e3dbb2a2435dc46327d772c5425c4b737eee88
zhangzhen <1094705507@qq.com>
2020-07-28 21:57:06 +0800

    集成安卓端支付宝支付

--- android/app/src/main/java/com/laundryapp/MainApplication.java
+++ android/app/src/main/java/com/laundryapp/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.laundryapp.alipay.AlipayPackage;
 import com.theweflex.react.WeChatPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new WeChatPackage()); // Add this line
+            packages.add(new AlipayPackage());
           return packages;
         }
 
